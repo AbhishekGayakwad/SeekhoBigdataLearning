8bc5c42ce23240fd337ec1b8d6e63f3f12b455af
AbhishekGayakwad <gayakwadabhishek86@gmail.com>
2024-10-25 12:34:07 +0530

    new changes

---
+++ src/main/scala/First.scala
@@ -0,0 +1,18 @@
+object First {
+  def main(args:Array[String]):Unit={
+    println("Hello World")
+
+    val a = 5
+    var b = 6
+    println(a)
+    println(b)
+    if(a == b)
+    {
+      println("Equal")
+    }
+    else
+    {
+      println("Not Equal")
+    }
+  }
+}
