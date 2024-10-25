5ff708eecfbd80d23596d16f77ff9776c3ebcc18
AbhishekGayakwad <gayakwadabhishek86@gmail.com>
2024-10-25 15:51:06 +0530

    new changes

--- src/main/scala/First.scala
+++ src/main/scala/First.scala
@@ -4,6 +4,7 @@ object First {
 
     val a = 5
     var b = 6
+    println(s"Check if the a "+a+" is equal to b "+b)
     println(a)
     println(b)
     if(a == b)
@@ -14,5 +15,12 @@ object First {
     {
       println("Not Equal")
     }
+    for (i<-1 to 10){
+      println(i)
+    }
+    for (i<-1 until 10){
+      println(i)
+    }
+
   }
 }
